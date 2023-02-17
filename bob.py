34ba2dd07a85d1ac80f47f3947b03b3c2ae931b1
mikekravets <michkravets@gmail.com>
2023-02-17 10:39:25 +0200

    Add bob function

---
+++ bob.py
@@ -0,0 +1,16 @@
+
+def bob(question: str) -> str:
+    if question == "what's up?":
+        return "Sure"
+    if question.isupper() and question.endswith("?"):
+        return "Relax, I know what I'm doing"
+    if question.isupper():
+        return "Wow relax"
+    if question == "":
+        return "OK, be so"
+    return "Whatever"
+
+
+if __name__ == '__main__':
+    res = bob("......")
+    print(res)
