372e1538f7e96c3e590e289cbd929f1597289046
mikekravets <michkravets@gmail.com>
2023-02-21 10:11:01 +0200

    Add function

--- pangam.py
+++ pangam.py
@@ -4,5 +4,14 @@ def is_pangram(s):
     return set('abcdefghijklmnopqrstuvwxyz') == set(s.lower().replace(' ', ''))
 
 
+def f(a=None):
+    if not a:
+        a = []
+    a.append(3)
+    return a
+
+
 if __name__ == "__main__":
-    print(is_pangram("The quick brown fox jumps over the lazy dog"))
+    n = [4, 5]
+    b = f()
+    c = f(n)
